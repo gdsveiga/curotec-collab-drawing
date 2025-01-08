6a7a43d7dab4b062bc02ede4d43bb3c5b71886b4
Guilherme Veiga <guilherme.dsveiga@gmail.com>
2025-01-08 14:13:57 -0300

    chore: setting up app.tsx with necessary providers, routers, suspense and toast container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth";
+import { Suspense } from "react";
+import LoadingBar from "./components/shared/loading-bar";
+import { ToastContainer } from "react-toastify";
+import Router from "./routes";
+
 function App() {
-  return <></>;
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Suspense fallback={<LoadingBar />}>
+          <Router />
+        </Suspense>
+        <ToastContainer />
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
 export default App;
