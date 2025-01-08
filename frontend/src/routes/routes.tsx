9dfda9b73e70b2574b2a054629946bb319c0f5ee
Guilherme Veiga <guilherme.dsveiga@gmail.com>
2025-01-08 14:08:38 -0300

    chore: routing setup

---
+++ frontend/src/routes/routes.tsx
@@ -0,0 +1,42 @@
+import { lazy } from "react";
+import { Navigate } from "react-router-dom";
+import PublicLayout from "src/layout/public";
+import ProtectedLayout from "src/layout/protected";
+
+// const LazyRegister = lazy(() => import("src/pages/register"));
+const LazyLogin = lazy(() => import("src/pages/login"));
+const LazyHome = lazy(() => import("src/pages/home"));
+
+const publicRoutes = [
+  {
+    path: "/login",
+    element: <LazyLogin />,
+  },
+  // {
+  //   path: "/register",
+  //   element: <LazyRegister />,
+  // },
+];
+const protectedRoutes = [
+  {
+    path: "/draw/home",
+    element: <LazyHome />,
+  },
+];
+
+export const routes = [
+  {
+    path: "/",
+    element: <PublicLayout />,
+    children: publicRoutes,
+  },
+  {
+    path: "/draw",
+    element: <ProtectedLayout />,
+    children: protectedRoutes,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/login" replace />,
+  },
+];
