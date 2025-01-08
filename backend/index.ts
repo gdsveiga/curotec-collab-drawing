6bd0a6f95b04086c4ad4f495b411bf1ae6e22173
Guilherme Veiga <guilherme.dsveiga@gmail.com>
2025-01-08 13:42:18 -0300

    chore: setting up necessary files and dependencies to start the server

---
+++ backend/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "src/config/db";
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+server.listen(4000, () => console.log("Server listening on port 4000"));
