db08a3936cec4473a4ab48c6b5c50c5684264973
Guilherme Veiga <guilherme.dsveiga@gmail.com>
2025-01-09 20:46:31 -0300

    chore: adding logout button to toolbar

--- frontend/src/components/drawing-board/toolbar.tsx
+++ frontend/src/components/drawing-board/toolbar.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { useDrawingContext } from "src/contexts/drawing";
-import { ArrowArcLeft, ArrowArcRight, File } from "@phosphor-icons/react";
+import {
+  ArrowArcLeft,
+  ArrowArcRight,
+  File,
+  SignOut,
+} from "@phosphor-icons/react";
 import { cn } from "src/utils/classname";
+import { useAuthContext } from "src/contexts/auth";
+import { useNavigate } from "react-router-dom";
 
 const Toolbar: React.FC = () => {
   const {
@@ -16,63 +23,82 @@ const Toolbar: React.FC = () => {
     handleRedo,
   } = useDrawingContext();
 
+  const { logout } = useAuthContext();
+  const navigate = useNavigate();
+
   return (
-    <div className="fixed bottom-4 w-[calc(100%-4rem)] lg:w-[calc(100%-20rem)] z-20 flex items-center gap-4 bg-stone-800 px-4 py-2 rounded">
+    <div className="fixed bottom-4 max-w-[calc(100%-4rem)] lg:max-w-[min(810px,100%)] w-full z-20 flex items-center justify-between bg-grass-800 px-4 py-2 rounded">
+      <div className="flex items-center gap-4">
+        <div
+          title="Clear canvas"
+          className="flex items-center justify-center p-1 rounded bg-grass-500 text-grass-700 cursor-pointer "
+          onClick={clearCanvas}
+        >
+          <File size={20} weight="bold" />
+        </div>
+        <div className="flex gap-1 items-center">
+          <div
+            title="Undo"
+            className={cn(
+              canUndo
+                ? "bg-grass-700 text-white cursor-pointer"
+                : "bg-neutral-700 text-neutral-400 cursor-not-allowed",
+              "flex items-center justify-center p-1 rounded "
+            )}
+            onClick={handleUndo}
+          >
+            <ArrowArcLeft size={20} weight="bold" />
+          </div>
+          <div
+            title="Redo"
+            className={cn(
+              canRedo
+                ? "bg-grass-700 text-white cursor-pointer"
+                : "bg-neutral-700 text-neutral-400 cursor-not-allowed",
+              "flex items-center justify-center p-1 rounded "
+            )}
+            onClick={handleRedo}
+          >
+            <ArrowArcRight size={20} weight="bold" />
+          </div>
+        </div>
+        <div className="flex items-center gap-8">
+          <div className="flex items-center gap-2">
+            <input
+              type="color"
+              title="Select color"
+              value={strokeColor}
+              onChange={(e) => setStrokeColor(e.target.value)}
+              className="bg-grass-800 border-none rounded"
+            />
+            <div className="text-grass-200 text-sm">{strokeColor}</div>
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              type="range"
+              min="2"
+              max="20"
+              value={strokeSize}
+              onChange={(e) => setStrokeSize(Number(e.target.value))}
+              title="Select brush size"
+              className="accent-grass-500"
+            />
+            <div className="text-grass-200 text-sm">{strokeSize}</div>
+          </div>
+        </div>
+      </div>
+
       <div
-        title="Clear canvas"
-        className="flex items-center justify-center p-1 rounded bg-emerald-600 cursor-pointer  text-white"
-        onClick={clearCanvas}
+        title="Redo"
+        className={
+          "bg-grass-500 text-grass-700 cursor-pointer flex items-center justify-center p-1 rounded "
+        }
+        onClick={() => {
+          navigate("/login");
+          logout();
+        }}
       >
-        <File size={20} weight="bold" />
-      </div>
-      <div className="flex gap-1 items-center">
-        <div
-          title="Undo"
-          className={cn(
-            canUndo
-              ? "bg-emerald-800 text-white cursor-pointer"
-              : "bg-neutral-700 text-neutral-400 cursor-not-allowed",
-            "flex items-center justify-center p-1 rounded "
-          )}
-          onClick={handleUndo}
-        >
-          <ArrowArcLeft size={20} weight="bold" />
-        </div>
-        <div
-          title="Redo"
-          className={cn(
-            canRedo
-              ? "bg-emerald-800 text-white cursor-pointer"
-              : "bg-neutral-700 text-neutral-400 cursor-not-allowed",
-            "flex items-center justify-center p-1 rounded "
-          )}
-          onClick={handleRedo}
-        >
-          <ArrowArcRight size={20} weight="bold" />
-        </div>
-      </div>
-      <div className="flex items-center gap-8">
-        <div className="flex items-center gap-2">
-          <input
-            type="color"
-            title="Select color"
-            value={strokeColor}
-            onChange={(e) => setStrokeColor(e.target.value)}
-            className="bg-stone-800 border-none rounded"
-          />
-          <div className="text-white text-sm">{strokeColor}</div>
-        </div>
-        <div className="flex items-center gap-2">
-          <input
-            type="range"
-            min="2"
-            max="20"
-            value={strokeSize}
-            onChange={(e) => setStrokeSize(Number(e.target.value))}
-            title="Select brush size"
-          />
-          <div className="text-white text-sm">{strokeSize}</div>
-        </div>
+        <SignOut size={20} weight="bold" />
       </div>
     </div>
   );
