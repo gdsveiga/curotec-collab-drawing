12329e240931448cb6d67cd6b56c501d5e675e5d
Guilherme Veiga <guilherme.dsveiga@gmail.com>
2025-01-09 20:47:21 -0300

    fix: fixing loading bar component size

--- frontend/src/components/shared/loading-bar.tsx
+++ frontend/src/components/shared/loading-bar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingBar: React.FC = () => {
   return (
-    <div className="w-full min-h-screen flex justify-center items-center">
+    <div className="w-[810px] min-h-screen mx-auto flex justify-center items-center">
       <div className="relative w-full h-2 bg-gray-200 overflow-hidden">
         <div className="absolute h-full bg-green-500 animate-loading-bar"></div>
       </div>
