5627f1174d76d9afa16b25b7a11eea1a598848e6
Guilherme Veiga <guilherme.dsveiga@gmail.com>
2025-01-09 20:46:50 -0300

    chore: adding interceptor to configure client authentication

--- frontend/src/config/client.ts
+++ frontend/src/config/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getLocalStorageItem } from "src/utils/localstorage";
 
 export const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL + "/api",
@@ -6,4 +7,12 @@ export const axiosClient = axios.create({
     "Content-Type": "application/json",
     Accept: "application/json",
   },
+});
+
+axiosClient.interceptors.request.use(function (config) {
+  const session = getLocalStorageItem("session");
+
+  if (session) config.headers.Authorization = `Bearer ${session.token}`;
+
+  return config;
 });
